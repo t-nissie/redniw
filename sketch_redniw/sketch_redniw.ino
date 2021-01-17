2783459ca861d31289396bc87fa36ba9184274e7
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2021-01-17 17:12:43 +0900

    add two more LEDs and one button

--- sketch_redniw/sketch_redniw.ino
+++ sketch_redniw/sketch_redniw.ino
@@ -6,17 +6,32 @@ const int    N_WIND_SW_PIN  = 1;
 LEDbits      n_wind_LED     = LEDbits();
 const int    N_WIND_LENGTH  = 6;
 const int    N_WIND_PINS[N_WIND_LENGTH]  = {3,5,6,9,10,11};
+
+Bounce       thickness_bouncer = Bounce();
+const int    THICKNESS_SW_PIN  = 12;
+LEDbits      thickness_LED     = LEDbits();
+const int    THICKNESS_LENGTH  = 2;
+const int    THICKNESS_PINS[THICKNESS_LENGTH]  = {13,8};
+
 void setup()
 {
   pinMode(N_WIND_SW_PIN, INPUT_PULLUP);
   n_wind_bouncer.attach(N_WIND_SW_PIN);
   n_wind_bouncer.interval(5);
   n_wind_LED.init(N_WIND_LENGTH, N_WIND_PINS);
+
+  pinMode(THICKNESS_SW_PIN, INPUT_PULLUP);
+  thickness_bouncer.attach(THICKNESS_SW_PIN);
+  thickness_bouncer.interval(5);
+  thickness_LED.init(THICKNESS_LENGTH, THICKNESS_PINS, 0);
 }
 
 void loop()
 {
   if (n_wind_bouncer.update() && n_wind_bouncer.read()==HIGH) {
     n_wind_LED.up();
+  }
+  if (thickness_bouncer.update() && thickness_bouncer.read()==HIGH) {
+    thickness_LED.up();
   }
 }
