4d13ca633a6d1ff5f6229e44ac26f69b70b6c23d
Takeshi Nishimatsu <t_nissie@yahoo.co.jp>
2021-01-23 09:14:05 +0900

    add a stepper motor

--- sketch_redniw/sketch_redniw.ino
+++ sketch_redniw/sketch_redniw.ino
@@ -18,6 +18,14 @@ const int SPOOL_PINS[SPOOL_LENGTH] = {17,18,19};
 const int PP_SW_PIN  = 14;
 Bounce    pp_bouncer = Bounce();   // play and pause
 
+const int GUIDE_STEP =  2;
+const int GUIDE_DIR  =  4;
+const int GUIDE_END  = 15;
+const double theta_step = 1.8;
+const int n_mode  = 8;
+const int n_2_0 = 2.0 * n_mode * 360.0 / theta_step;
+const int n_2_5 = 2.5 * n_mode * 360.0 / theta_step;
+
 void setup()
 {
   pinMode(  N_WIND_SW_PIN, INPUT_PULLUP);
@@ -43,6 +51,31 @@ void setup()
   pinMode(PP_SW_PIN, INPUT_PULLUP);
   pp_bouncer.attach(PP_SW_PIN);
   pp_bouncer.interval(5);
+
+  pinMode(GUIDE_STEP,OUTPUT);
+  pinMode(GUIDE_DIR, OUTPUT);
+  pinMode(GUIDE_END,  INPUT); // endstop
+
+  digitalWrite(GUIDE_DIR,0);   // to the endstep direction
+  do {
+        digitalWrite(GUIDE_STEP,0);
+        delayMicroseconds(1000);
+        digitalWrite(GUIDE_STEP,1);
+        delayMicroseconds(1000);
+  } while (digitalRead(GUIDE_END));
+
+  delay(1000); // Wait 1 seconds
+  digitalWrite(GUIDE_DIR,1); // Set Dir
+  for (int i = 0; i < n_2_5; i++) {
+    for (int k = 0; k < 2; k++) {
+      digitalWrite(GUIDE_STEP,k);
+      delayMicroseconds(500);
+    }
+  }
+
+  n_wind_LED.set_bits(0);
+  thickness_LED.set_bits(0);
+  spool_LED.set_bits(0);
 
   do {
     if (   n_wind_bouncer.update() &&    n_wind_bouncer.read()==HIGH)    n_wind_LED.up();
